7337209eba942d749a66b94d0b32f512308bd337
Shashin Gupta <41521876+Shashin-Gupta@users.noreply.github.com>
2020-01-04 23:05:45 -0800

    Coordinator

---
+++ Coordinator.java
@@ -0,0 +1,12 @@
+package java.eyeLight;
+
+public class Coordinator {
+
+	public static final String WILDFIRE_PATH = "java/Wildfire_Information";
+	public static final String HURRICANE_PATH = "java/Hurricane_Information";
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
