0c074219b9d54f815609ef8848f437559b299002
Shashin Gupta <41521876+Shashin-Gupta@users.noreply.github.com>
2020-01-09 15:06:47 -0800

    DataSet

---
+++ DataSet.java
@@ -0,0 +1,20 @@
+package java.eyeLight;
+
+public class DataSet {
+
+	private int year, data;
+	
+	public DataSet(int year, int data) {
+		this.year = year;
+		this.data = data;
+	}
+	
+	public int getYear() {
+		return year;
+	}
+	
+	public int getData() {
+		return data;
+	}
+	
+}
